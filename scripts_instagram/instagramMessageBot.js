3142f107d1cc7d6c9db881249b6a954f30e9c757
RajveerKalsi <rajveer.k@anarix.io>
2025-10-29 19:09:09 +0530

    added setUserAgent

--- scripts_instagram/instagramMessageBot.js
+++ scripts_instagram/instagramMessageBot.js
@@ -128,6 +128,9 @@ ${contact_name}`;
     defaultViewport: null,
   });
   const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+  );
   await loginInstagramWithVerification(page, username, password);
 
   let sentCount = 0;
